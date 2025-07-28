92afb0bc8520f92a0cb20178a112f005455cc4d0
clive <clivetoniato1@hotmail.com>
2025-07-28 18:48:08 +0100

    removed kata as i want to improve knowledge

--- 6 Kuy/Linked_Lists_Recursive_Reverse.js
+++ 6 Kuy/Linked_Lists_Recursive_Reverse.js
@@ -1,40 +1,30 @@
 function Node(data, head = null) {
-	this.data = data === undefined ? null : data;
-	this.next = head;
+  this.data = data === undefined ? null : data;
+  this.next = head;
 }
 
 function buildList(arr) {
-	if (arr.length === 0) return null;
+  if (arr.length === 0) return null;
 
-	const head = new Node(arr[0]);
-	let current = head;
+  const head = new Node(arr[0]);
+  let current = head;
 
-	for (let i = 1; i < arr.length; i++) {
-		current.next = new Node(arr[i]);
-		current = current.next;
-	}
+  for (let i = 1; i < arr.length; i++) {
+    current.next = new Node(arr[i]);
+    current = current.next;
+  }
 
-	return head;
+  return head;
 }
 
-function reverse(head) {
-	// Your code goes here.
-	// if(head)
-	if (!head.next) return head;
 
-	if (head.next) {
-		reverse(head.next);
+// const list = buildList([3, 8, 1]);
+// console.log(
+//   reverse(list),
+//   buildList([1, 8, 3]),
+//   "result should be 1 -> 8 -> 3 -> null."
+// );
 
-		head.next.next = head;
-    head.next = null;
-	}
-
-	return head;
-}
-
-const list = buildList([3, 8, 1]);
-console.log(
-	reverse(list),
-	buildList([1, 8, 3]),
-	"result should be 1 -> 8 -> 3 -> null."
-);
+// console.log(reverse(buildList([3, 1])), buildList([1, 3]));
+console.log(reverse(buildList([1, 2, 3])), buildList([1,2,3]));
+// console.log(reverse(null),null);
