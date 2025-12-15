57d14da6d4cac514108865be0e4e63f18849d386
clivepato93 <52717551+clivepato93@users.noreply.github.com>
2025-12-15 15:19:01 +0000

    Create sortList.js

---
+++ 7 Kuy/sortList.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/52705ed65de62b733f000064/train/javascript
+
+// Arrays
+// Sorting
+// Fundamentals
+
+function sortList (sortBy, list) {
+  return list.sort((a,b)=> b[sortBy]- a[sortBy]);
+}
