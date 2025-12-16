27b0178de58077005eaad804f46c410c05e85377
clivepato93 <52717551+clivepato93@users.noreply.github.com>
2025-12-16 14:35:15 +0000

    Create alphabetized.js

---
+++ 6 Kuy/alphabetized.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5970df092ef474680a0000c9/train/javascript
+// Strings
+// Sorting
+
+// generate an array holding only a-z/i
+// compare each letter in lowercase form
+
+function alphabetized(s) {
+  console.log(s)
+  const arr = []
+  for(let i = 0;i <s.length;i++){
+    if(/[a-z]/i.test(s[i])) arr.push(s[i])
+  }
+  return arr.sort((a,b)=> a.toLowerCase().charCodeAt(0)-b.toLowerCase().charCodeAt(0)).join('')
+}
