77ebc7e018d2c2a20612ab9385b5dbb7e0f6cc6c
clivepato93 <52717551+clivepato93@users.noreply.github.com>
2026-01-09 10:00:00 +0000

    Create canSantaSaveChristmas.js

---
+++ canSantaSaveChristmas.js
@@ -0,0 +1,15 @@
+// split the string 
+// i could do a subtract from 24hrs in seconds
+// as long as the seconds is greater than minus that should work
+function determineTime (durations) {
+  //have fun with coding ^.^
+  let seconds = 86400;
+  for(let i =0;i<durations.length;i++){
+    const [hour,mins,secs] = durations[i].split(':');
+    seconds -= hour*3600;
+    seconds -= mins*60;
+    seconds -= secs;
+  }
+  
+  return seconds>=0
+}
