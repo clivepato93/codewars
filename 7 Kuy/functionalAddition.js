403051e17e359280354b40ead3937a997464a749
clive pato <clivepato@clives-MacBook-Pro.local>
2026-02-01 16:55:35 +0000

    functionAddition

---
+++ 7 Kuy/functionalAddition.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/538835ae443aae6e03000547/train/javascript
+// Functional Programming
+// Fundamentals
+
+function add(n) {
+  return function(num){
+    return n + num
+  }
+}
