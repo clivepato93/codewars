26650343d62f1171cd346aeeda9e4bb0bad65686
clive <52717551+clivepato93@users.noreply.github.com>
2026-02-20 14:51:09 +0000

    Create basicVariableAssignment.js

---
+++ basicVariableAssignment.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/javascript
+// Debugging
+/*
+var a == "code";
+var b == "wa.rs";
+var name == a + b; 
+
+*/
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
